a07478600dc50d6e35336f9c9e3bf0cab5899061
Арцименя Александр 350504 <sasha22parol@gmail.com>
2025-10-30 03:31:35 +0300

    deleted trash

--- test_login.ps1
+++
@@ -1,6 +0,0 @@
-$body = '{"username": "admin", "password": "admin123"}' | ConvertTo-Json
-$response = Invoke-WebRequest -Uri http://localhost:8000/api/v1/token/ -Method POST -Body $body -ContentType 'application/json' -UseBasicParsing
-$content = $response.Content | ConvertFrom-Json
-Write-Host "Access token:" $content.access
-Write-Host "User:" $content.user.username
-Write-Host "Can manage articles:" $content.user.can_manage_articles
