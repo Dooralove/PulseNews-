5e45077f9964a243ed7a4ff4e0833bac5cc71b3e
Арцименя Александр 350504 <sasha22parol@gmail.com>
2025-10-30 03:30:41 +0300

    deleted trash

--- check_api.ps1
+++
@@ -1,4 +0,0 @@
-$response = Invoke-WebRequest -Uri http://localhost:8000/api/v1/ -UseBasicParsing
-$content = $response.Content | ConvertFrom-Json
-Write-Host "Available endpoints:"
-$content | ConvertTo-Json -Depth 3
