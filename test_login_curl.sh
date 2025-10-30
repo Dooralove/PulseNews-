7e1f01a8d5bfbef79a8255072f2f875fa118b997
Арцименя Александр 350504 <sasha22parol@gmail.com>
2025-10-30 03:28:25 +0300

    deleted trash

--- test_login_curl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Testing login via curl..."
-curl -X POST http://localhost:8000/api/v1/auth/login/ \
-  -H "Content-Type: application/json" \
-  -d '{"username": "admin", "password": "admin123"}' \
-  -v
